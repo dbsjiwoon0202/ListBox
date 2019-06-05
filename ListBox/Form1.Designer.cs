25ce3fda9bae5239d4802aadb9b69f57ebbe3451
Jiwon Yoon <s2017i33@e-mirim.hs.kr>
2019-06-05 10:41:34 +0900

    #1 listbox 추가

--- ListBox/Form1.Designer.cs
+++ ListBox/Form1.Designer.cs
@@ -28,13 +28,72 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.idView = new System.Windows.Forms.ListBox();
+            this.ivlResult = new System.Windows.Forms.Label();
+            this.txtList = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // idView
+            // 
+            this.idView.FormattingEnabled = true;
+            this.idView.ItemHeight = 12;
+            this.idView.Location = new System.Drawing.Point(12, 12);
+            this.idView.Name = "idView";
+            this.idView.Size = new System.Drawing.Size(120, 88);
+            this.idView.TabIndex = 0;
+            this.idView.SelectedIndexChanged += new System.EventHandler(this.ListBox1_SelectedIndexChanged);
+            // 
+            // ivlResult
+            // 
+            this.ivlResult.AutoSize = true;
+            this.ivlResult.Location = new System.Drawing.Point(13, 107);
+            this.ivlResult.Name = "ivlResult";
+            this.ivlResult.Size = new System.Drawing.Size(33, 12);
+            this.ivlResult.TabIndex = 1;
+            this.ivlResult.Text = "결과:";
+            this.ivlResult.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // txtList
+            // 
+            this.txtList.Location = new System.Drawing.Point(15, 123);
+            this.txtList.Name = "txtList";
+            this.txtList.Size = new System.Drawing.Size(117, 21);
+            this.txtList.TabIndex = 2;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(12, 151);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(120, 62);
+            this.btnAdd.TabIndex = 3;
+            this.btnAdd.Text = "추가";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(323, 322);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtList);
+            this.Controls.Add(this.ivlResult);
+            this.Controls.Add(this.idView);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "리스트 추";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox idView;
+        private System.Windows.Forms.Label ivlResult;
+        private System.Windows.Forms.TextBox txtList;
+        private System.Windows.Forms.Button btnAdd;
     }
 }
 
--- ListBox/Form1.cs
+++ ListBox/Form1.cs
@@ -16,5 +16,15 @@ namespace ListBox
         {
             InitializeComponent();
         }
+
+        private void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
